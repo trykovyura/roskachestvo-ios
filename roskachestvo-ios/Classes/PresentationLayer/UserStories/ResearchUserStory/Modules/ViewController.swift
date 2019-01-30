7ab9a974577d418af66e57ba6c2d6745036e3f56
trykov <me@trykov.ru>
2019-01-30 12:36:56 +0300

    initial commit

---
+++ roskachestvo-ios/Classes/PresentationLayer/UserStories/ResearchUserStory/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  roskachestvo-ios
+//
+//  Created by Юрий Трыков on 30.01.2019.
+//  Copyright © 2019 trykov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
